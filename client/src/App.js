7f64ae0f14fd954b425f654133a2d9fdb067e8c1
Nico Rithner <nicorithner@gmail.com>
2022-06-06 07:44:38 -0600

    Add routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,23 @@
-import './App.css';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Home from "./pages/Home.js";
+import Register from "./pages/Register.js";
+import Login from "./pages/Login.js";
+
+import "./App.css";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <h1>Welcome to subscription app</h1>
-        <div className="lead">Using React Nodejs Stripe and MongoDB</div>
-      </div>
-    </div>
+  root.render(
+    <BrowserRouter>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/register" element={<Register />} />
+        <Route exact path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
