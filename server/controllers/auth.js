77202398824f8ace5f73f7242f77837464c2f64c
Nico Rithner <nicorithner@gmail.com>
2022-06-06 15:02:07 -0600

    Add basic validation

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,3 +1,22 @@
-export const register = (_req, res) => {
-  res.json({ data: "Hello from auth controller" });
+export const register = async (req, res) => {
+  try {
+    // validation
+    const { name, password } = req.body;
+    if (!name) {
+      return res.json({
+        error: "Name is required",
+      });
+    }
+
+    if (!password || password.length < 6) {
+      return res.json({
+        error: "Password is required and should be longer than 6 characters",
+      });
+    }
+    res.json({
+      data: "This is /register endpoint",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
