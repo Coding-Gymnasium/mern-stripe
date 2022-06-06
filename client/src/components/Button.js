5ddd0ee775f0f1c0202d2597a8c7296e48a4aade
Nico Rithner <nicorithner@gmail.com>
2022-06-06 12:24:21 -0600

    Create button component

---
+++ client/src/components/Button.js
@@ -0,0 +1,14 @@
+const Button = ({
+  type = "primary",
+  size = "md",
+  text = "Submit",
+  handleClick,
+}) => {
+  return (
+    <button onClick={handleClick} className={`btn btn-${type} btn-${size}`}>
+      {text}
+    </button>
+  );
+};
+
+export default Button;
