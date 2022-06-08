9dc78a4d719875774756e1a4facb7b0360c86e9b
Nico Rithner <nicorithner@gmail.com>
2022-06-08 09:19:07 -0600

    Add password verification and connect to database

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,7 +1,10 @@
+import User from "../models/user.js";
+import { hashPassword, comparePassword } from "../helpers/auth.js";
+
 export const register = async (req, res) => {
   try {
     // validation
-    const { name, password } = req.body;
+    const { name, email, password } = req.body;
     if (!name) {
       return res.json({
         error: "Name is required",
@@ -13,9 +16,28 @@ export const register = async (req, res) => {
         error: "Password is required and should be longer than 6 characters",
       });
     }
-    res.json({
-      data: "This is /register endpoint",
-    });
+
+    const exist = await User.findOne({ email });
+    if (exist) {
+      return res.json({
+        error: "Email is taken",
+      });
+    }
+
+    // hash password
+    const hashedPassword = await hashPassword(password);
+    try {
+      const user = await new User({
+        name,
+        email,
+        password: hashedPassword,
+      }).save();
+
+      const { password, ...rest } = user._doc;
+      return res.json(user);
+    } catch (err) {
+      console.log(err);
+    }
   } catch (err) {
     console.log(err);
   }
