99c40c862c22c4da130f43022ed5c78f40bf014a
Nico Rithner <nicorithner@gmail.com>
2022-06-15 06:37:53 -0600

    Add Login route

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,9 @@ import express from "express";
 
 const router = express.Router();
 
-import { register } from "../controllers/auth.js";
+import { login, register } from "../controllers/auth.js";
 
 router.post("/register", register);
+router.post('/login', login);
 
 module.exports = router;
