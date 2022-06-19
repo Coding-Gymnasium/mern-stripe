e13dcfdd5746f29f9f9ab15487c4dd5100581b92
Nico Rithner <nicorithner@gmail.com>
2022-06-19 15:21:07 -0600

    Add context

--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,13 +1,18 @@
+import {useContext} from 'react';
 import { Link, useNavigate } from "react-router-dom";
 import { isAuth } from "../utils/functions.js";
+import {UserContext} from '../context'
 
 const Navigation = () => {
+  const [state, setState] = useContext(UserContext);
   let navigate = useNavigate();
 
   const logout = () => {
     localStorage.removeItem("auth");
     navigate("/login");
   };
+
+  console.log('STATE => ', state);
 
   return (
     <ul className="nav border">
