5c2d73b173bc7c7add75b7b6db7e44beddf8e9dd
Nico Rithner <nicorithner@gmail.com>
2022-06-22 09:05:11 -0600

    Add component to home page

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,13 +1,16 @@
+import PriceCard from "../components/cards/PriceCard.js";
+
 const Home = () => {
   return (
-    <div className="d-flex justify-content-center" style={{ height: "80vh" }}>
-      <div className="container align-items-center d-flex">
-        <div className="row col-md-6 offset-md-3 text-center">
-          <h1 className="pt-5 fw-bold">Home</h1>
-          <p className="lead pb-4">
-            ...
-          </p>
-        </div>
+    <div className="container-fluid">
+      <div className="row col-md-6 offset-md-3 text-center">
+        <h1 className="pt-5 fw-bold">Explore Plans</h1>
+        <p className="lead pb-4">Find the right plan for your business</p>
+      </div>
+      <div className="row pt-5 mb-3 text-center">
+        <PriceCard />
+        <PriceCard />
+        <PriceCard />
       </div>
     </div>
   );
