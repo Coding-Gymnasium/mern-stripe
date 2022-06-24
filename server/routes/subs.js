b5cce5f615079631094d886cd8f2b9945195ad55
Nico Rithner <nicorithner@gmail.com>
2022-06-24 11:56:15 -0600

    Create post route to create subscription

--- server/routes/subs.js
+++ server/routes/subs.js
@@ -2,8 +2,10 @@ import express from "express";
 
 const router = express.Router();
 
-import { prices } from "../controllers/subs.js";
+import { prices, createSubscription } from "../controllers/subs.js";
+import {requireSignin} from '../middlewares';
 
 router.get("/prices", prices);
+router.post('/create-subscription', requireSignin, createSubscription);
 
 module.exports = router;
