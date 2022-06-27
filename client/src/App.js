5d3e9c15df6bba92040bf26a954019a49295f1d5
Nico Rithner <nicorithner@gmail.com>
2022-06-27 08:46:33 -0600

    Remove 'exact' as is no longer needed

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,12 @@
-// import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Toaster} from 'react-hot-toast';
+import { Toaster } from "react-hot-toast";
 
 import Home from "./pages/Home.js";
 import Register from "./pages/Register.js";
 import Login from "./pages/Login.js";
-import Navigation from './components/Navigation.js';
-import StripeSuccess from './pages/stripe-success.js';
-import StripeCancel from './pages/stripe-cancel.js';
+import Navigation from "./components/Navigation.js";
+import StripeSuccess from "./pages/stripe-success.js";
+import StripeCancel from "./pages/stripe-cancel.js";
 
 import "./App.css";
 
@@ -15,15 +14,18 @@ function App() {
   return (
     <BrowserRouter>
       <Navigation />
-      <Toaster position="bottom-right" toastOptions={{
-        duration: 3000,
-      }}/>
+      <Toaster
+        position="bottom-right"
+        toastOptions={{
+          duration: 3000,
+        }}
+      />
       <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/register" element={<Register />} />
-        <Route exact path="/login" element={<Login />} />
-        <Route exact path="/stripe/success" element={<StripeSuccess />} />
-        <Route exact path="/stripe/cancel" element={<StripeCancel />} />
+        <Route path="/" element={<Home />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/stripe/success" element={<StripeSuccess />} />
+        <Route path="/stripe/cancel" element={<StripeCancel />} />
       </Routes>
     </BrowserRouter>
   );
